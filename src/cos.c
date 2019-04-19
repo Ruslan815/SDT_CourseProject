2367626b86bc9dfd0d8f4dbb1b3b870765a84233
Daria Ogloblina <ogloblik@yandex.ru>
2019-04-19 18:27:02 +0700

    Fixed bugs in cos.c

--- src/cos.c
+++ src/cos.c
@@ -6,7 +6,8 @@ double cosinus(double x)
     double n = 1.0;
     double cos = 0.0;
     int i = 1;
-
+    while (x > 2 * 3.14159265)
+        x -= 2 * 3.141592;
     do {
         cos += n;
         n *= -1.0 * x * x / ((2 * i - 1) * (2 * i));
