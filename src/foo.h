d0418fc14b9f4d743993a32ff357249cc5df0e93
Daria Ogloblina <ogloblik@yandex.ru>
2019-04-19 19:48:02 +0700

    Adding a function for exponentiation to the header file

--- src/foo.h
+++ src/foo.h
@@ -3,6 +3,7 @@
 double sinus(double x);
 double cosinus(double x);
 double tan(double x);
-double ctan(double x);
+double cotan(double x);
 int factorial(int x);
+double exponentiation(double x, int n);
 #endif
